bc3b288c00566d0fa3d994b3271b562dd4500687
Valmir Júnior <valmirgmj@gmail.com>
2024-11-07 02:22:00 -0300

    Adds a global theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,18 @@
 import '@/styles/globals.css'
+import { ThemeProvider } from '@emotion/react'
+import { createTheme } from '@mui/material';
+import { ptBR } from '@mui/x-date-pickers/locales'
+
+const theme = createTheme(
+  {
+    palette: {
+      primary: { main: '#1976d2' },
+    },
+  },
+  ptBR
+);
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={theme}><Component {...pageProps} /></ThemeProvider>
 }
